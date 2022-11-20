b74e91cecbd9b1a3dcae60acf31e8996dec03016
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2022-11-20 22:23:56 +0900

    キャッチコピーとコンセプトを正しく表示できるように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,6 +25,12 @@ class PrototypesController < ApplicationController
   end
 
   def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else 
+      render :edit
+    end
 
   end
 
