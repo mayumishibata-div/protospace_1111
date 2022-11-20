118672117a7c3aaa04e6376ad214ace0862a9401
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2022-11-20 23:40:56 +0900

    コントローラーにcreateアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @prototype = Prototype.find(params[:id])
+    @comment = @prototype.comments.new(comment_params)
+    @comment.save
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype.id])
+  end
 end
